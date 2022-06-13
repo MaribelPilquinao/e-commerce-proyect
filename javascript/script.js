f750b8dff6c8b455a133946549af2232731d8946
Jeanpol Giraldo <giraldojeanpool@hotmail.com>
2022-06-13 11:32:03 -0500

    fixed frame1

--- javascript/script.js
+++ javascript/script.js
@@ -2,24 +2,24 @@
 
 let product = [
 
-    {productImg:'<img src="./images/product-1.png" class ="product-img"></img>',productImage: "image1", productItem: "Sudadera Academlo", productColor: "Logo rojo, color gris", productPrice: "$12.000", divNro: "div1"},
-    {productImg:'<img src="./images/product-2.png" class ="product-img"></img>' ,productImage: "image2", productItem: "Camisa Academlo", productColor: "Logo blanco, color rojo", productPrice: "$2.000", divNro: "div2"},
-    {productImg:'<img src="./images/product-3.png" class ="product-img"></img>' ,productImage: "image3", productItem: "Sudadera Academlo", productColor: "Logo rojo, color blanco", productPrice: "$12.000", divNro: "div3"},
-    {productImg:'<img src="./images/product-4.png" class ="product-img"></img>' ,productImage: "image4", productItem: "Sudadera Academlo", productColor: "Logo blanco, color rojo", productPrice: "$12.000", divNro: "div4"},
-    {productImg:'<img src="./images/product-5.png" class ="product-img"></img>' ,productImage: "image5", productItem: "Camisa Academlo", productColor: "Logo blanco, color negro", productPrice: "$2.000", divNro: "div5"},
-    {productImg:'<img src="./images/product-5.png" class ="product-img"></img>' ,productImage: "image6", productItem: "Camisa Academlo", productColor: "Logo blanco, color negro", productPrice: "$2.000", divNro: "div6"},
-    {productImg:'<img src="./images/product-4.png" class ="product-img"></img>' ,productImage: "image7", productItem: "Sudadera Academlo", productColor: "Logo blanco, color rojo", productPrice: "$12.000", divNro: "div7"},
-    {productImg:'<img src="./images/product-6.png" class ="product-img"></img>' ,productImage: "image8", productItem: "Sudadera Academlo", productColor: "Logo negro, color gris", productPrice: "$12.000", divNro: "div8"},
-    {productImg:'<img src="./images/product-7.png" class ="product-img"></img>' ,productImage: "image9", productItem: "Camisa Academlo", productColor: "Logo rojo, color blanco", productPrice: "$2.000", divNro: "div9"},
-    {productImg:'<img src="./images/product-8.png" class ="product-img"></img>' ,productImage: "image10", productItem: "Sudadera Academlo", productColor: "Logo blanco, color negro", productPrice: "$12.000", divNro: "div10"},
-    {productImg:'<img src="./images/product-1.png" class ="product-img"></img>' ,productImage: "image11", productItem: "Sudadera Academlo", productColor: "Logo rojo, color gris", productPrice: "$12.000", divNro: "div11"},
-    {productImg:'<img src="./images/product-2.png" class ="product-img"></img>' ,productImage: "image12", productItem: "Camisa Academlo", productColor: "Logo blanco, color rojo", productPrice: "$2.000", divNro: "div12"},
-    {productImg:'<img src="./images/product-3.png" class ="product-img"></img>' ,productImage: "image13", productItem: "Sudadera Academlo", productColor: "Logo rojo, color blanco", productPrice: "$12.000", divNro: "div13"},
-    {productImg:'<img src="./images/product-4.png" class ="product-img"></img>' ,productImage: "image14", productItem: "Sudadera Academlo", productColor: "Logo blanco, color rojo", productPrice: "$12.000", divNro: "div14"},
-    {productImg:'<img src="./images/product-5.png" class ="product-img"></img>' ,productImage: "image15", productItem: "Camisa Academlo", productColor: "Logo blanco, color negro", productPrice: "$2.000", divNro: "div15"},    
+    { id: 1, productImg:'<img src="./images/product-1.png" class ="product-img"></img>',productImage: "image1", productItem: "Sudadera Academlo", productColor: "Logo rojo, color gris", productPrice: "$12.000", divNro: "div1"},
+    { id: 2, productImg:'<img src="./images/product-2.png" class ="product-img"></img>' ,productImage: "image2", productItem: "Camisa Academlo", productColor: "Logo blanco, color rojo", productPrice: "$2.000", divNro: "div2"},
+    { id: 3, productImg:'<img src="./images/product-3.png" class ="product-img"></img>' ,productImage: "image3", productItem: "Sudadera Academlo", productColor: "Logo rojo, color blanco", productPrice: "$12.000", divNro: "div3"},
+    { id: 4, productImg:'<img src="./images/product-4.png" class ="product-img"></img>' ,productImage: "image4", productItem: "Sudadera Academlo", productColor: "Logo blanco, color rojo", productPrice: "$12.000", divNro: "div4"},
+    { id: 5, productImg:'<img src="./images/product-5.png" class ="product-img"></img>' ,productImage: "image5", productItem: "Camisa Academlo", productColor: "Logo blanco, color negro", productPrice: "$2.000", divNro: "div5"},
+    { id: 6, productImg:'<img src="./images/product-5.png" class ="product-img"></img>' ,productImage: "image6", productItem: "Camisa Academlo", productColor: "Logo blanco, color negro", productPrice: "$2.000", divNro: "div6"},
+    { id: 7, productImg:'<img src="./images/product-4.png" class ="product-img"></img>' ,productImage: "image7", productItem: "Sudadera Academlo", productColor: "Logo blanco, color rojo", productPrice: "$12.000", divNro: "div7"},
+    { id: 8, productImg:'<img src="./images/product-6.png" class ="product-img"></img>' ,productImage: "image8", productItem: "Sudadera Academlo", productColor: "Logo negro, color gris", productPrice: "$12.000", divNro: "div8"},
+    { id: 9, productImg:'<img src="./images/product-7.png" class ="product-img"></img>' ,productImage: "image9", productItem: "Camisa Academlo", productColor: "Logo rojo, color blanco", productPrice: "$2.000", divNro: "div9"},
+    { id: 10, productImg:'<img src="./images/product-8.png" class ="product-img"></img>' ,productImage: "image10", productItem: "Sudadera Academlo", productColor: "Logo blanco, color negro", productPrice: "$12.000", divNro: "div10"},
+    { id: 11, productImg:'<img src="./images/product-1.png" class ="product-img"></img>' ,productImage: "image11", productItem: "Sudadera Academlo", productColor: "Logo rojo, color gris", productPrice: "$12.000", divNro: "div11"},
+    { id: 12, productImg:'<img src="./images/product-2.png" class ="product-img"></img>' ,productImage: "image12", productItem: "Camisa Academlo", productColor: "Logo blanco, color rojo", productPrice: "$2.000", divNro: "div12"},
+    { id: 13, productImg:'<img src="./images/product-3.png" class ="product-img"></img>' ,productImage: "image13", productItem: "Sudadera Academlo", productColor: "Logo rojo, color blanco", productPrice: "$12.000", divNro: "div13"},
+    { id: 14, productImg:'<img src="./images/product-4.png" class ="product-img"></img>' ,productImage: "image14", productItem: "Sudadera Academlo", productColor: "Logo blanco, color rojo", productPrice: "$12.000", divNro: "div14"},
+    { id: 15, productImg:'<img src="./images/product-5.png" class ="product-img"></img>' ,productImage: "image15", productItem: "Camisa Academlo", productColor: "Logo blanco, color negro", productPrice: "$2.000", divNro: "div15"},    
 ]
 
-let productoRelacionado = [
+let productRelated= [
 
     {productImg:'<img src="./images/product-1.png" class ="product-img"></img>',productImage: "image1", productItem: "Sudadera Academlo", productColor: "Logo rojo, color gris", productPrice: "$12.000", divNro: "div1"},
     {productImg:'<img src="./images/product-2.png" class ="product-img"></img>' ,productImage: "image2", productItem: "Camisa Academlo", productColor: "Logo blanco, color rojo", productPrice: "$2.000", divNro: "div2"},
@@ -47,22 +47,93 @@ function cardGenerator(array) {
                 </div>
                 <p>${array[i].productItem}</p>
                 <p>${array[i].productColor}</p>
+                <p>ID: ${array[i].id}</p>
                 <span>${array[i].productPrice}</span>
-                <button class="card-buttom" type="button">agregar al carrito</button>
+                <button class="card-buttom" type="button" onclick="addCart(${array[i].id})">agregar al carrito</button>
             </div>
         
         `;
     }
 
     const container = document.getElementById("products-container")
-    console.log(container)
-    console.log("hola")
     container.innerHTML = html
+}
+
+
+cardGenerator(product)
+// cardGenerator(productRelated)
+
+
+
+
+//-----------------------------------------------------------------------------------------------------------------------------------//
+
+// funciones principales para callbacks.
+
+function filter(array, cb) {
+    let filteredData = [];
+    for(let i = 0; i < array.length; i++) {
+        let result = cb(array[i]);
+        if(result) {
+            filteredData.push(array[i]);
+        }
+    }
+    return filteredData;
+}
+
+function find(array, cb) {
+    for(let i=0; i<array.length; i++) {
+        let result = cb(array[i]);
+        if(result===true) {
+            return array[i];
+        }
+    }
+}
+
+
+//-----------------------------------------------------------------------------------------------------------------------------------//
+
+
+
+
+let shoppingCart = []
+
+let contador = 0
+
+
+
+
+function addCart (id) {
+
+    shoppingCart = []
+
+    function cbFindId (product){
+        return product.id === id
+    }
+
+    let productFinded = find(product,cbFindId);
+
+    shoppingCart.push(productFinded)
+
+    console.log(shoppingCart)
+
 }
 
 
 
 
-cardGenerator(product)
 
-cardGenerator(productoRelacionado)
+
+
+
+
+function ContadorItems() {
+    contador++
+    console.log(contador)
+}
+
+
+
+
+
+window.addCart = addCart
